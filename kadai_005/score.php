39011d416d14ce6a9813ef0ebf0a6db2411c9f67
n-studio-firma <159144250+n-studio-firma@users.noreply.github.com>
2024-07-10 12:00:54 +0900

    Update score.php

--- kadai_005/score.php
+++ kadai_005/score.php
@@ -20,6 +20,12 @@
     $score9 = 30;
     $score10 = 60;
 
+    $score_total = $score1 + $score2 + $score3 + $score4 + $score5 + $score6 + $score7 + $score8 + $score9 + $score10;
+
+    echo $score_total;
+
+    echo "<br>";
+
     // 変数と数値を計算し、その結果を出力する
     echo ($score1 + $score2 + $score3 + $score4 + $score5 + $score6 + $score7 + $score8 + $score9 + $score10) / 10;
     ?>
