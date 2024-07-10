f2353b18f0c644d0889ab951d18b40d8605405ea
n-studio-firma <159144250+n-studio-firma@users.noreply.github.com>
2024-07-10 18:34:34 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>kadai_007</title>
+</head>
+<body>
+
+<p>
+  <?php
+// 連想配列に値を代入する
+  $item_date = ["name" => "onion", 'price'=> 200, "weight" => 160];
+
+  // 連想配列の値を出力する
+  print_r($item_date);
+
+  ?>
+</p>
+
+  
+</body>
+</html>
